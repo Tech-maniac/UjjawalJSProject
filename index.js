66b36e75d66df34dfa9a98a1cc854d806f45ffd1
Tech-maniac <57858304+Tech-maniac@users.noreply.github.com>
2022-12-09 13:47:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('outputtext');
+let entryCount = 0;
+let entryLimit = 3;
+let error = false;
+
+Attempts = 3;
+
+let number = [Math.floor(Math.random()*10)]
+console.log("The number is : "+number);
+
+attemptID = document.getElementById('Attempts');
+attemptID.innerHTML = `Attempts left : ${Attempts}`;
+
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('userInput').value;
+    console.log("The number you entered is : "+input);
+    if (input == number)
+    {
+        output.innerHTML= `You guessed right number, your number was ${number}`;
+        document.body.style.backgroundColor = 'green';
+    }
+    // while(input != number) {
+    //     output.innerHTML= 'You guessed wrong number';
+    //     entryCount= entryCount++;}
+
+    //     while(entryCount = entryLimit) {
+    //         document.getElementById("userInput").disabled = true;
+    //     }
+    else if (input != number){
+        output.innerHTML= 'You guessed wrong number';
+        document.body.style.backgroundColor = 'red';
+
+        Attempts = Attempts - 1;
+        console.log("Attempts left : "+Attempts);
+
+        attemptID.innerHTML = `Attempts left : ${Attempts}`;
+
+        if(Attempts==0)
+        {
+            document.getElementById("btn").disabled = true;
+            NoAttemptsLeftId= document.getElementById('NoAttemptsLeft');
+            NoAttemptsLeftId.innerHTML="Oops ! You ran out of attempts :("
+        }
+
+        //for(let i= 0;i< entryLimit; i++) {
+            //entryCount= i;
+            //console.log(entryCount);
+            // while(entryCount=2){
+            //     document.getElementById('userOuptut').disabled = true;
+            //     output.innerHTML = 'You have reached the maximum attempts, you cannot guess more'
+            // }
+        //}
+    };
+    // if(input > number) {
+    //     output.innerHTML= 'You guessed too high';
+    //     document.body.style.backgroundColor = 'red';
+    //     entryCount= entryCount++
+    // };
+})
+console.log(entryCount)
